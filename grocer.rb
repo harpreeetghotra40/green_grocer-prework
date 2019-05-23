74292a48860ba4277ff218d0a43d3978f067a944
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-05-23 22:31:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,29 @@
-def consolidate_cart(cart)
+ef consolidate_cart(cart:[])
+  result = {}
   # code here
+  
+  result
 end
 
-def apply_coupons(cart, coupons)
-  # code here
+def apply_single_coupon(cart, coupon)
+
+   coupItem = coupon[:item]
+
+   if cart.keys.include?(coupItem) && cart[coupItem][:count] >= coupon[:num]
+    cart[coupItem][:count] = cart[coupItem][:count] - coupon[:num]
+    cart["#{coupItem} W/COUPON"] ||= {}
+    cart["#{coupItem} W/COUPON"][:price] = coupon[:cost]
+    cart["#{coupItem} W/COUPON"][:clearance] = cart[coupItem][:clearance]
+    cart["#{coupItem} W/COUPON"][:count] ||= 0
+    cart["#{coupItem} W/COUPON"][:count] += 1
+  end
 end
 
-def apply_clearance(cart)
-  # code here
+def apply_coupons(cart: [], coupons: [])
+  # code here	  coupons.each do |couponHash|
+    apply_single_coupon(cart, couponHash)
+  end
+  cart
 end
 
 def checkout(cart, coupons)
